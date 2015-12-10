6db5d29069f43e12a66060e0ff6ab3da62db00f4
India Dearlove <india_dearlove@yahoo.co.uk>
2015-12-10 11:41:37 +0000

    form validation nodeauth

--- nodeauth/app.js
+++ nodeauth/app.js
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //Handle file uploads
-app.use(multer({dest:'./uploads/'}).single('upload'));
+app.use(multer({dest:'./uploads/'}).any());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
